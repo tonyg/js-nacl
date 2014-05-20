ec4caa17f56be9e298b5a26fc45405a7ecfb75b5
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-05-20 16:49:29 -0400

    Ed25519

--- tweetnacl.js
+++ tweetnacl.js
@@ -130,6 +130,12 @@ function TweetNaclC() {
 	this.hi = other.hi;
     };
 
+    Word.prototype.seti = function (lo, hi) {
+	hi = this.extendHi(lo, hi);
+	this.lo = lo >>> 0;
+	this.hi = hi >>> 0;
+    };
+
     Word.prototype.load = function (arr, ofs) {
 	this.lo = arr[(ofs << 1) + 0];
 	this.hi = arr[(ofs << 1) + 1];
@@ -283,8 +289,17 @@ function TweetNaclC() {
 	this.hi = (~this.hi) >>> 0;
     };
 
+    Word.prototype.neg = function () {
+	this.not();
+	this.addi(1);
+    };
+
     // -=-=-=- END int64array -=-=-=-
     ///////////////////////////////////////////////////////////////////////////
+
+    function new_gf_zero() {
+	return new_int64array(16);
+    }
 
     function new_gf(vs) {
 	var g = new_int64array(16);
@@ -303,7 +318,7 @@ function TweetNaclC() {
     var _0 = new Uint8Array(16);
     var _9 = new Uint8Array(32); _9[0] = 9;
 
-    var gf0 = new_gf([]);
+    var gf0 = new_gf_zero();
     var gf1 = new_gf([1]);
     var _121665 = new_gf([0xDB41, 1]);
     var D = new_gf([0x78a3, 0x1359, 0x4dca, 0x75eb, 0xd8ab, 0x4141, 0x0a4d, 0x0070, 0xe898, 0x7779, 0x4079, 0x8cc7, 0xfe73, 0x2b6f, 0x6cee, 0x5203]);
@@ -648,7 +663,7 @@ function TweetNaclC() {
 
     function pack25519(o,n) {
 	var i, j, b;
-	var m = new_gf([]);
+	var m = new_gf_zero();
 	var t = copy_gf(n);
 	var tmp = new Word();
 
@@ -778,11 +793,11 @@ function TweetNaclC() {
 	var z = new Uint8Array(32);
 	var x = new_int64array(80);
 	var i, r;
-	var a = new_gf([]);
-	var c = new_gf([]);
-	var d = new_gf([]);
-	var e = new_gf([]);
-	var f = new_gf([]);
+	var a = new_gf_zero();
+	var c = new_gf_zero();
+	var d = new_gf_zero();
+	var e = new_gf_zero();
+	var f = new_gf_zero();
 	z.set(n);
 	z[31]=(n[31]&127)|64;
 	z[0]&=248;
@@ -1132,232 +1147,310 @@ function TweetNaclC() {
     }
 
     ///////////////////////////////////////////////////////////////////////////
+    // ed25519
 
-/*
-static void add(gf p[4],gf q[4])
-{
-  gf a,b,c,d,t,e,f,g,h;
-  
-  Z(a, p[1], p[0]);
-  Z(t, q[1], q[0]);
-  M(a, a, t);
-  A(b, p[0], p[1]);
-  A(t, q[0], q[1]);
-  M(b, b, t);
-  M(c, p[3], q[3]);
-  M(c, c, D2);
-  M(d, p[2], q[2]);
-  A(d, d, d);
-  Z(e, b, a);
-  Z(f, d, c);
-  A(g, d, c);
-  A(h, b, a);
+    function new_gf4() { return [new_gf_zero(), new_gf_zero(), new_gf_zero(), new_gf_zero()]; }
 
-  M(p[0], e, f);
-  M(p[1], h, g);
-  M(p[2], g, f);
-  M(p[3], e, h);
-}
+    function add(p,q) {
+	var a = new_gf_zero();
+	var b = new_gf_zero();
+	var c = new_gf_zero();
+	var d = new_gf_zero();
+	var t = new_gf_zero();
+	var e = new_gf_zero();
+	var f = new_gf_zero();
+	var g = new_gf_zero();
+	var h = new_gf_zero();
 
-static void cswap(gf p[4],gf q[4],u8 b)
-{
-  int i;
-    for (i = 0; i < 4; i++)
-    sel25519(p[i],q[i],b);
-}
+	Z(a, p[1], p[0]);
+	Z(t, q[1], q[0]);
+	M(a, a, t);
+	A(b, p[0], p[1]);
+	A(t, q[0], q[1]);
+	M(b, b, t);
+	M(c, p[3], q[3]);
+	M(c, c, D2);
+	M(d, p[2], q[2]);
+	A(d, d, d);
+	Z(e, b, a);
+	Z(f, d, c);
+	A(g, d, c);
+	A(h, b, a);
 
-static void pack(u8 *r,gf p[4])
-{
-  gf tx, ty, zi;
-  inv25519(zi, p[2]); 
-  M(tx, p[0], zi);
-  M(ty, p[1], zi);
-  pack25519(r, ty);
-  r[31] ^= par25519(tx) << 7;
-}
-
-static void scalarmult(gf p[4],gf q[4],const u8 *s)
-{
-  int i;
-    p[0].set(gf0);
-    p[1].set(gf1);
-    p[2].set(gf1);
-    p[3].set(gf0);
-  for (i = 255;i >= 0;--i) {
-    u8 b = (s[i/8]>>(i&7))&1;
-    cswap(p,q,b);
-    add(q,p);
-    add(p,p);
-    cswap(p,q,b);
-  }
-}
-
-static void scalarbase(gf p[4],const u8 *s)
-{
-  gf q[4];
-    q[0].set(X);
-    q[1].set(Y);
-    q[2].set(gf1);
-  M(q[3],X,Y);
-  scalarmult(p,q,s);
-}
-
-int crypto_sign_keypair(u8 *pk, u8 *sk)
-{
-  u8 d[64];
-  gf p[4];
-  int i;
-
-  randombytes(sk, 32);
-  crypto_hash(d, sk, 32);
-  d[0] &= 248;
-  d[31] &= 127;
-  d[31] |= 64;
-
-  scalarbase(p,d);
-  pack(pk,p);
-
-    for (i = 0; i < 32; i++) sk[32 + i] = pk[i];
-  return 0;
-}
-
-static const u64 L[32] = {0xed, 0xd3, 0xf5, 0x5c, 0x1a, 0x63, 0x12, 0x58, 0xd6, 0x9c, 0xf7, 0xa2, 0xde, 0xf9, 0xde, 0x14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x10};
-
-static void modL(u8 *r,i64 x[64])
-{
-  i64 carry,i,j;
-  for (i = 63;i >= 32;--i) {
-    carry = 0;
-    for (j = i - 32;j < i - 12;++j) {
-      x[j] += carry - 16 * x[i] * L[j - (i - 32)];
-      carry = (x[j] + 128) >> 8;
-      x[j] -= carry << 8;
+	M(p[0], e, f);
+	M(p[1], h, g);
+	M(p[2], g, f);
+	M(p[3], e, h);
     }
-    x[j] += carry;
-    x[i] = 0;
-  }
-  carry = 0;
-    for (j = 0; j < 32; j++) {
-    x[j] += carry - (x[31] >> 4) * L[j];
-    carry = x[j] >> 8;
-    x[j] &= 255;
-  }
-    for (j = 0; j < 32; j++) x[j] -= carry * L[j];
-    for (i = 0; i < 32; i++) {
-    x[i+1] += x[i] >> 8;
-    r[i] = x[i] & 255;
-  }
-}
 
-static void reduce(u8 *r)
-{
-  i64 x[64],i;
-    for (i = 0; i < 64; i++) x[i] = (u64) r[i];
-    for (i = 0; i < 64; i++) r[i] = 0;
-  modL(r,x);
-}
+    function cswap(p,q,b) {
+	for (var i = 0; i < 4; i++)
+	    sel25519(p[i],q[i],b);
+    }
 
-int crypto_sign(u8 *sm,u64 *smlen,const u8 *m,u64 n,const u8 *sk)
-{
-  u8 d[64],h[64],r[64];
-  i64 i,j,x[64];
-  gf p[4];
+    function pack(r,p) {
+	var tx = new_gf_zero();
+	var ty = new_gf_zero();
+	var zi = new_gf_zero();
+	inv25519(zi, p[2]); 
+	M(tx, p[0], zi);
+	M(ty, p[1], zi);
+	pack25519(r, ty);
+	r[31] ^= par25519(tx) << 7;
+    }
 
-  crypto_hash(d, sk, 32);
-  d[0] &= 248;
-  d[31] &= 127;
-  d[31] |= 64;
+    function scalarmult(p,q,s) {
+	p[0].set(gf0);
+	p[1].set(gf1);
+	p[2].set(gf1);
+	p[3].set(gf0);
+	for (var i = 255;i >= 0;--i) {
+	    var b = (s[i/8]>>(i&7))&1;
+	    cswap(p,q,b);
+	    add(q,p);
+	    add(p,p);
+	    cswap(p,q,b);
+	}
+    }
 
-  *smlen = n+64;
-    for (i = 0; i < n; i++) sm[64 + i] = m[i];
-    for (i = 0; i < 32; i++) sm[32 + i] = d[32 + i];
+    function scalarbase(p,s)
+    {
+	var q = new_gf4();
+	q[0].set(X);
+	q[1].set(Y);
+	q[2].set(gf1);
+	M(q[3],X,Y);
+	scalarmult(p,q,s);
+    }
 
-  crypto_hash(r, sm+32, n+32);
-  reduce(r);
-  scalarbase(p,r);
-  pack(sm,p);
+    function crypto_sign_keypair(pk,sk) {
+	var d = new Uint8Array(64);
+	var p = new_gf4();
+	var i;
 
-    for (i = 0; i < 32; i++) sm[i+32] = sk[i+32];
-  crypto_hash(h,sm,n + 64);
-  reduce(h);
+	randombytes(sk, 32);
+	crypto_hash(d, sk, 32);
+	d[0] &= 248;
+	d[31] &= 127;
+	d[31] |= 64;
 
-    for (i = 0; i < 64; i++) x[i] = 0;
-    for (i = 0; i < 32; i++) x[i] = (u64) r[i];
-    for (i = 0; i < 32; i++) for (j = 0; j < 32; j++) x[i+j] += h[i] * (u64) d[j];
-  modL(sm + 32,x);
+	scalarbase(p,d);
+	pack(pk,p);
 
-  return 0;
-}
+	for (i = 0; i < 32; i++) sk[32 + i] = pk[i];
+	return 0;
+    }
 
-static int unpackneg(gf r[4],const u8 p[32])
-{
-  gf t, chk, num, den, den2, den4, den6;
-    r[2].set(gf1);
-  unpack25519(r[1],p);
-  S(num,r[1]);
-  M(den,num,D);
-  Z(num,num,r[2]);
-  A(den,r[2],den);
+    var L = [
+	new Word(0xed, 0), new Word(0xd3, 0), new Word(0xf5, 0), new Word(0x5c, 0),
+	new Word(0x1a, 0), new Word(0x63, 0), new Word(0x12, 0), new Word(0x58, 0),
+	new Word(0xd6, 0), new Word(0x9c, 0), new Word(0xf7, 0), new Word(0xa2, 0),
+	new Word(0xde, 0), new Word(0xf9, 0), new Word(0xde, 0), new Word(0x14, 0),
+	new Word(0, 0), new Word(0, 0), new Word(0, 0), new Word(0, 0),
+	new Word(0, 0), new Word(0, 0), new Word(0, 0), new Word(0, 0),
+	new Word(0, 0), new Word(0, 0), new Word(0, 0), new Word(0, 0),
+	new Word(0, 0), new Word(0, 0), new Word(0, 0), new Word(0x10, 0)
+    ];
 
-  S(den2,den);
-  S(den4,den2);
-  M(den6,den4,den2);
-  M(t,den6,num);
-  M(t,t,den);
+    function modL(r,x) { // u8 *r,i64 x[64]
+	var carry = new Word();
+	var tmp = new Word();
+	var i, j;
+	
+	for (i = 63;i >= 32;--i) {
+	    carry.zero();
+	    for (j = i - 32;j < i - 12;++j) {
+		tmp.seti(16);
+		tmp.mul_load(x, i);
+		tmp.mul_load(L, j - (i - 32));
+		tmp.neg();
+		tmp.add(carry);
+		tmp.add_load(x, j);
+		tmp.store(x, j);
 
-  pow2523(t,t);
-  M(t,t,num);
-  M(t,t,den);
-  M(t,t,den);
-  M(r[0],t,den);
+		carry.set(tmp);
+		carry.addi(128);
+		carry.sari(8);
 
-  S(chk,r[0]);
-  M(chk,chk,den);
-  if (neq25519(chk, num)) M(r[0],r[0],I);
+		tmp.set(carry);
+		tmp.shli(8);
+		tmp.neg();
+		tmp.add_load(x, j);
+		tmp.store(x, j)
+	    }
+	    carry.add_load(x, j);
+	    carry.store(x, j);
+	    setlo32(x, i, 0);
+	    sethi32(x, i, 0);
+	}
+	carry.zero();
+	for (j = 0; j < 32; j++) {
+	    tmp.load(x, 31);
+	    tmp.sari(4);
+	    tmp.mul_load(L, j);
+	    tmp.neg();
+	    tmp.add(carry);
+	    tmp.add_load(x, j);
 
-  S(chk,r[0]);
-  M(chk,chk,den);
-  if (neq25519(chk, num)) return -1;
+	    carry.set(tmp);
+	    carry.sari(8);
 
-  if (par25519(r[0]) == (p[31]>>7)) Z(r[0],gf0,r[0]);
+	    tmp.andi(255);
+	    tmp.store(x, j);
+	}
+	for (j = 0; j < 32; j++) {
+	    tmp.set(carry);
+	    tmp.mul_load(L, j);
+	    tmp.neg();
+	    tmp.add_load(x, j);
+	    tmp.store(x, j);
+	}
+	for (i = 0; i < 32; i++) {
+	    tmp.load(x, i);
+	    tmp.sari(x, 8);
+	    tmp.add_load(x, i+1);
+	    tmp.store(x, i+1);
+	    r[i] = getlo32(x, i) & 255;
+	}
+    }
 
-  M(r[3],r[0],r[1]);
-  return 0;
-}
+    function reduce(r) { // u8 *r
+	var x = new_int64array(64);
+	var i;
+	for (i = 0; i < 64; i++) setlo32(x, i, r[i]);
+	for (i = 0; i < 64; i++) r[i] = 0;
+	modL(r,x);
+    }
 
-int crypto_sign_open(u8 *m,u64 *mlen,const u8 *sm,u64 n,const u8 *pk)
-{
-  int i;
-  u8 t[32],h[64];
-  gf p[4],q[4];
+    function crypto_sign(sm,smlen,m,n,sk) { // u8 *sm,u64 *smlen,const u8 *m,u64 n,const u8 *sk
+	var d = new Uint8Array(64);
+	var h = new Uint8Array(64);
+	var r = new Uint8Array(64);
+	var i, j;
+	var x = new_int64array(64);
+	var p = new_gf4();
 
-  *mlen = -1;
-  if (n < 64) return -1;
+	crypto_hash(d, sk, 32);
+	d[0] &= 248;
+	d[31] &= 127;
+	d[31] |= 64;
 
-  if (unpackneg(q,pk)) return -1;
+	smlen.seti(n+64);
+	for (i = 0; i < n; i++) sm[64 + i] = m[i];
+	for (i = 0; i < 32; i++) sm[32 + i] = d[32 + i];
 
-    for (i = 0; i < n; i++) m[i] = sm[i];
-    for (i = 0; i < 32; i++) m[i+32] = pk[i];
-  crypto_hash(h,m,n);
-  reduce(h);
-  scalarmult(p,q,h);
+	crypto_hash(r, sm.subarray(32), n+32);
+	reduce(r);
+	scalarbase(p,r);
+	pack(sm,p);
 
-  scalarbase(q,sm + 32);
-  add(p,q);
-  pack(t,p);
+	for (i = 0; i < 32; i++) sm[i+32] = sk[i+32];
+	crypto_hash(h,sm,n + 64);
+	reduce(h);
 
-  n -= 64;
-  if (crypto_verify_32(sm, t)) {
-      for (i = 0; i < n; i++) m[i] = 0;
-    return -1;
-  }
+	for (i = 0; i < 32; i++) setlo32(x, i, r[i]);
+	for (i = 0; i < 32; i++) {
+	    for (j = 0; j < 32; j++) {
+		setlo32(x, i+j, getlo32(x, i+j) + (h[i] * d[j]));
+	    }
+	}
+	modL(sm.subarray(32),x);
 
-    for (i = 0; i < n; i++) m[i] = sm[i + 64];
-  *mlen = n;
-  return 0;
-}
+	return 0;
+    }
 
-*/
+    function unpackneg(r,p) { // gf r[4],const u8 p[32]
+	var t = new_gf_zero();
+	var chk = new_gf_zero();
+	var num = new_gf_zero();
+	var den = new_gf_zero();
+	var den2 = new_gf_zero();
+	var den4 = new_gf_zero();
+	var den6 = new_gf_zero();
+
+	r[2].set(gf1);
+	unpack25519(r[1],p);
+	S(num,r[1]);
+	M(den,num,D);
+	Z(num,num,r[2]);
+	A(den,r[2],den);
+
+	S(den2,den);
+	S(den4,den2);
+	M(den6,den4,den2);
+	M(t,den6,num);
+	M(t,t,den);
+
+	pow2523(t,t);
+	M(t,t,num);
+	M(t,t,den);
+	M(t,t,den);
+	M(r[0],t,den);
+
+	S(chk,r[0]);
+	M(chk,chk,den);
+	if (neq25519(chk, num)) M(r[0],r[0],I);
+
+	S(chk,r[0]);
+	M(chk,chk,den);
+	if (neq25519(chk, num)) return -1;
+
+	if (par25519(r[0]) == (p[31]>>7)) Z(r[0],gf0,r[0]);
+
+	M(r[3],r[0],r[1]);
+	return 0;
+    }
+
+    function crypto_sign_open(m,mlen,sm,n,pk) { // u8 *m,u64 *mlen,const u8 *sm,u64 n,const u8 *pk
+	var i;
+	var t = new Uint8Array(32);
+	var h = new Uint8Array(64);
+	var p = new_gf4();
+	var q = new_gf4();
+
+	mlen.seti(-1);
+	if (n < 64) return -1;
+
+	if (unpackneg(q,pk)) return -1;
+
+	for (i = 0; i < n; i++) m[i] = sm[i];
+	for (i = 0; i < 32; i++) m[i+32] = pk[i];
+	crypto_hash(h,m,n);
+	reduce(h);
+	scalarmult(p,q,h);
+
+	scalarbase(q,sm.subarray(32));
+	add(p,q);
+	pack(t,p);
+
+	n -= 64;
+	if (crypto_verify_32(sm, t)) {
+	    for (i = 0; i < n; i++) m[i] = 0;
+	    return -1;
+	}
+
+	for (i = 0; i < n; i++) m[i] = sm[i + 64];
+	mlen.seti(n);
+	return 0;
+    }
+
+    // addition to the core tweetnacl library
+    function crypto_sign_keypair_from_raw_sk(pk, sk, seed) {
+	var p = new_gf4();
+	var i;
+
+	crypto_hash(sk, seed, 32);
+	sk[0] &= 248;
+	sk[31] &= 63;
+	sk[31] |= 64;
+
+	scalarbase(p, sk);
+	pack(pk, p);
+
+	for (i = 0; i < 32; i++) sk[i] = seed[i];
+	for (i = 0; i < 32; i++) sk[i + 32] = pk[i];
+	return 0;
+    }
 
     return {
 	encode_utf8: encode_utf8,
@@ -1385,6 +1478,10 @@ int crypto_sign_open(u8 *m,u64 *mlen,const u8 *sm,u64 n,const u8 *pk)
 	crypto_box_open: crypto_box_open,
 	crypto_hashblocks: crypto_hashblocks,
 	crypto_hash: crypto_hash,
+	crypto_sign_keypair: crypto_sign_keypair,
+	crypto_sign: crypto_sign,
+	crypto_sign_open: crypto_sign_open,
+	crypto_sign_keypair_from_raw_sk: crypto_sign_keypair_from_raw_sk,
 
 	constants: {
 	    crypto_auth_PRIMITIVE: "hmacsha512256",
@@ -1699,24 +1796,73 @@ function TweetNacl() {
 
 	///////////////////////////////////////////////////////////////////////////
 
-/*
-    function crypto_sign_keypair_from_seed(bs) {
-	var seeda = check_injectBytes("crypto_sign_keypair_from_seed",
-				      "seed", bs, nacl_raw._crypto_sign_SECRETKEYBYTES / 2);
-	var pk = new Target(nacl_raw._crypto_sign_PUBLICKEYBYTES);
-	var sk = new Target(nacl_raw._crypto_sign_SECRETKEYBYTES);
-	check("_crypto_sign_keypair_from_raw_sk",
-	      nacl_raw._crypto_sign_keypair_from_raw_sk(pk.address, sk.address, seeda));
-	FREE(seeda);
-	return {signPk: pk.extractBytes(), signSk: sk.extractBytes()};
-    }
+	crypto_sign_keypair: function () {
+	    var pk = new Uint8Array(C.crypto_sign_PUBLICKEYBYTES);
+	    var sk = new Uint8Array(C.crypto_sign_SECRETKEYBYTES);
+	    check("crypto_sign_keypair", nacl_raw.crypto_sign_keypair(pk, sk));
+	    return {signPk: pk, signSk: sk};
+	},
 
-    function crypto_box_keypair_from_seed(bs) {
-	var hash = new Uint8Array(crypto_hash(bs));
-	return crypto_box_keypair_from_raw_sk(hash.subarray(0,
-							    nacl_raw._crypto_box_SECRETKEYBYTES));
-    }
-*/
+	crypto_sign: function (msg, sk) {
+	    var ma = coerce_u8(msg);
+	    var ska = check_length("crypto_sign", "sk", sk, C.crypto_sign_SECRETKEYBYTES);
+	    var sm = new Uint8Array(msg.byteLength + C.crypto_sign_BYTES);
+	    var smlen = new Word();
+	    check("crypto_sign", nacl_raw.crypto_sign(sm, smlen, ma, msg.byteLength, ska));
+	    if (smlen.hi !== 0) {
+		throw {message: "crypto_sign: signed message longer than 2^32-1 bytes"};
+	    }
+	    return sm.subarray(0, smlen.lo);
+	},
+
+	crypto_sign_detached: function (msg, sk) {
+	    // WARNING: Experimental. Works for ed25519 but not necessarily other implementations.
+	    var signed_msg = crypto_sign(msg, sk);
+	    return signed_msg.subarray(0, C.crypto_sign_BYTES);
+	},
+
+	crypto_sign_open: function (sm, pk) {
+	    var sma = coerce_u8(sm);
+	    var pka = check_length("crypto_sign_open", "pk", pk, C.crypto_sign_PUBLICKEYBYTES);
+	    var m = new Uint8Array(sm.byteLength);
+	    var mlen = new Word();
+	    if (nacl_raw.crypto_sign_open(m, mlen, sma, sm.byteLength, pka) === 0) {
+		if (mlen.hi !== 0) {
+		    throw {message: "crypto_sign_open: message longer than 2^32-1 bytes"};
+		}
+		return m.subarray(0, mlen.lo);
+	    } else {
+		return null;
+	    }
+	},
+
+	crypto_sign_verify_detached: function (detached_signature, msg, pk) {
+	    // WARNING: Experimental. Works for ed25519 but not necessarily other implementations.
+	    var det = coerce_u8(detached_signature);
+	    var m = coerce_u8(msg);
+	    var signed_msg = new Uint8Array(det.byteLength + m.byteLength);
+	    signed_msg.set(det, 0);
+	    signed_msg.set(m, det.byteLength);
+	    return crypto_sign_open(signed_msg, pk) !== null;
+	},
+
+	///////////////////////////////////////////////////////////////////////////
+
+	crypto_sign_keypair_from_seed: function (bs) {
+	    var seeda = check_length("crypto_sign_keypair_from_seed", "seed", bs,
+				     C.crypto_sign_SECRETKEYBYTES / 2);
+	    var pk = new Uint8Array(C.crypto_sign_PUBLICKEYBYTES);
+	    var sk = new Uint8Array(C.crypto_sign_SECRETKEYBYTES);
+	    check("crypto_sign_keypair_from_raw_sk",
+		  nacl_raw.crypto_sign_keypair_from_raw_sk(pk, sk, seeda));
+	    return {signPk: pk, signSk: sk};
+	},
+
+	crypto_box_keypair_from_seed: function (bs) {
+	    var hash = new Uint8Array(exports.crypto_hash(bs));
+	    var sk = hash.subarray(0, C.crypto_box_SECRETKEYBYTES);
+	    return exports.crypto_box_keypair_from_raw_sk(sk);
+	},
 
 	crypto_box_keypair_from_raw_sk: function (sk) {
 	    return {boxPk: exports.crypto_scalarmult_base(sk), boxSk: sk};
